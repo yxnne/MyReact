6600fcdb9f7419a4ec99acc6ff506fab0eed3440
iel-mac <506524339@qq.com>
2018-02-02 16:13:08 +0800

    添加了mongoose链接mongodb并测试增删改查

--- testing_express/server/server.js
+++ testing_express/server/server.js
@@ -1,4 +1,43 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// mongodb 的 url
+// const DB_URL = 'mongodb://127.0.0.1:27017';
+const DB_URL = 'mongodb://127.0.0.1:27017/test'; // test要是没有会新建,test是集合
+// mongoose 去链接数据库
+mongoose.connect(DB_URL);
+mongoose.connection.on('connected', function(){
+  console.log("mongodb has connected and I noticed it ~~");
+  console.log("mongoose is helpful ~~~~");
+});
+
+// 类似于建立 MySQL 的表,文档模型
+const User = mongoose.model('user', new mongoose.Schema({
+  name:{type:String, require:true},
+  age:{type:Number,require:true}
+}));
+
+// 新建数据
+User.create({
+  name:"ppp",
+  age:5
+},(err, doc)=>{
+  if (!err){
+    console.log(doc);
+  }else{
+    console.log(err);
+  }
+});
+
+// 删除age是18的对象
+User.remove({age:18},(e, d)=>{
+  console.log(d);
+});
+
+// 更新名字是jerk的
+User.update({name:"jerk"}, {'$set':{age:77}}, (err, doc)=>{
+  console.log(doc);
+});
 
 // 新建app
 const app = express();
@@ -16,8 +55,12 @@ app.get('/', (require, response) =>{
 // '/json'
 app.get('/json', (require, response) =>{
   // 用response对象
-  response.json({
-    type:"test",
-    name:"express"
+  // response.json({
+  //   type:"test",
+  //   name:"express"
+  // });
+
+  User.find({},(err, doc)=>{
+    response.json(doc);
   });
 });
